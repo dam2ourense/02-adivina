ef8f28a43f5c86db959aed7ff2158f4103c3d4a6
unknown <luis@ACARBALLEIRA.LOCAL>
2018-10-10 13:46:42 +0200

    inicial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+    //tipos primitvos number, string, boolean, any, Array
+    num:number;    
+    mayorMenor : string = "...";
+    numSecreto : number = this.numAleatorio(0,10);
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  numAleatorio (a,b){
+    return Math.round(Math.random()*(b-a)+parseInt(a));
+  }
+
+  compruebaNumero (){
+    if (this.num){
+      if (this.numSecreto < this.num){
+        this.mayorMenor = 'menor de';
+      }
+      else if (this.numSecreto>this.num){
+        this.mayorMenor = "mayor de";
+      }
+      else{
+        this.mayorMenor = '';
+      }
+    }
+  }
+
+  reinicia(){
+    this.num = null;
+    this.mayorMenor = "...";
+    this.numSecreto = this.numAleatorio(1,10);
+  }
+
+}
